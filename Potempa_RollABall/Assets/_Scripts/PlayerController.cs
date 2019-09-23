b54628f171652c835964be66308483f6f36e1e71
mappotem <mappotem@iu.edu>
2019-09-23 00:20:37 -0400

    added text objects

--- Potempa_RollABall/Assets/_Scripts/PlayerController.cs
+++ Potempa_RollABall/Assets/_Scripts/PlayerController.cs
@@ -1,22 +1,23 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
 
     public float speed;
+    public Text countText;
+    public Text winText;
     private Rigidbody rb;
-    // Start is called before the first frame update
+    private int count;
+ 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-    }
+        count = 0;
+        SetCountText();
+        winText.text = "";
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     private void FixedUpdate()
@@ -34,6 +35,18 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 12)
+        {
+            winText.text = "You Win!";
         }
     }
 }
